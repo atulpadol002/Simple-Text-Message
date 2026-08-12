361bda47ec5e36f489af0489f0d6dd18f2f0c563
Atul Padol <atulpadol002@gmail.com>
2026-08-12 23:51:34 +0530

    Fix AndroidX Fragment dependency for release lint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.fragment)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
 
